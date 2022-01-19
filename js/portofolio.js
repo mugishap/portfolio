6f75ed09ba372a9463e263454fb5fd50de72e652
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-01-19 07:12:31 +0200

    initial commit

---
+++ js/portofolio.js
@@ -0,0 +1,14 @@
+let text = "Hello there👋👋👋, I am Mugisha Puceau Precieux. Nice to meet you, below there is my PORTOFOLIO."
+
+let theInterval = setInterval(playText, 200)
+let i = 0
+
+function playText(){
+    document.getElementById('playtext').textContent += text[i]
+    
+    i++
+    if(i == text.length){
+        clearInterval(theInterval)
+    }
+} 
+
